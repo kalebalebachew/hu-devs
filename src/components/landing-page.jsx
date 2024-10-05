bd014e50ca08ae5af7b084ea09963171df64e9fd
Kaleb <Kalebalebachew4@gmail.com>
2024-10-05 16:31:14 +0300

    chore: some adjustments regarding fonts and navbar

--- src/components/landing-page.jsx
+++ src/components/landing-page.jsx
@@ -15,23 +15,29 @@ import Link from "next/link";
 export default function LandingPage() {
   return (
     <div className="flex flex-col min-h-screen bg-white text-black font-nunito">
-      <header className="px-4 lg:px-6 h-16 flex items-center bg-blue-800 text-white">
+      <header className="px-4 lg:px-6 h-16 flex items-center bg-white sticky text-blue-700">
         <a className="flex items-center justify-center" href="#">
           <Code className="h-6 w-6" />
           <span className="ml-2 text-lg font-bold">HUDC</span>
         </a>
-        <nav className="ml-auto flex gap-4 sm:gap-6">
+        <nav className="ml-auto flex  gap-4 sm:gap-6">
           <a
-            className="text-sm font-medium hover:underline underline-offset-4"
+            className="text-sm font-bold hover:underline underline-offset-4"
             href="#features"
           >
             why join us
           </a>
           <a
-            className="text-sm font-medium hover:underline underline-offset-4"
+            className="text-sm font-bold hover:underline underline-offset-4"
             href="#about"
           >
-            About
+            about
+          </a>
+          <a
+            className="text-sm font-bold hover:underline underline-offset-4"
+            href="#contact"
+          >
+            contact
           </a>
         </nav>
       </header>
