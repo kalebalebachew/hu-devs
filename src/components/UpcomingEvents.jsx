e5e59116757b83017126d921072559fa4c53bade
Kaleb <Kalebalebachew4@gmail.com>
2024-10-13 14:45:26 +0300

    style: some style adjustments

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -1,43 +1,42 @@
 import Image from "next/image";
 
 const events = [
-    {
-      title: "Practical Insights for Aspiring Tech Professionals",
-      time: "Sunday, October 20 2:30 LT",
-      location: "HUDC Telegram Channel",
-      img: "/john.jpg",
-    },
-  ];
-  
-  export function UpcomingEventsTab() {
-    return (
-      <section className="p-8">
-        <h2 className="text-3xl font-bold mb-8">UPCOMING EVENTS</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 ">
-          {events.map((event, index) => (
-            <div
-              key={index}
-              className="border rounded-lg overflow-hidden shadow-lg text-start border-purple-500"
-            >
-              <Image
-                src={event.img} 
-                alt={event.title}
-                width={1000}
-                height={96}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
-                <h3 className="text-xl font-semibold">{event.title}</h3>
-                <p className="text-purple-600 font-semibold">{event.time}</p>
-                <p className="text-white">{event.location}</p>
-                <button className="mt-4 px-4 py-2 bg-purple-500 text-black hover:bg-purple-700 rounded-sm">
-                  VIEW DETAIL &rarr;
-                </button>
-              </div>
+  {
+    title: "Practical Insights for Aspiring Tech Professionals",
+    time: "Sunday, October 20 2:30 LT",
+    location: "HUDC Telegram Channel",
+    img: "/john.jpg",
+  },
+];
+
+export function UpcomingEventsTab() {
+  return (
+    <section className="p-4 sm:p-12">
+      <h2 className="text-2xl sm:text-3xl font-bold mb-6 sm:mb-8">Upcoming Events</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-6 sm:gap-8">
+        {events.map((event, index) => (
+          <div
+            key={index}
+            className="border rounded-lg overflow-hidden shadow-lg text-start border-purple-500 border-opacity-40 bg-gray-800"
+          >
+            <Image
+              src={event.img} 
+              alt={event.title}
+              width={1000}
+              height={96}
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h3 className="text-lg sm:text-xl font-semibold">{event.title}</h3>
+              <p className="text-purple-600 font-semibold">{event.time}</p>
+              <p className="text-white">{event.location}</p>
+              <button className="mt-4 w-full px-4 py-2 bg-purple-500 text-black hover:bg-purple-700 rounded-md">
+                View details &rarr;
+              </button>
             </div>
-          ))}
-        </div>
-      </section>
-    );
-  }
-  
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
