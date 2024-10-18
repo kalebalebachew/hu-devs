305c1ea791c81e2baf3e541e9c41d61a1ee47381
naPOME <natipomebiz1234@gmail.com>
2024-10-18 22:45:40 +0300

    Add pages directory with separate folders

---
+++ src/app/pages/student/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Student = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <h1 className="text-2xl font-bold">Student Dashboard</h1>
+    </header>
+  );
+};
+
+export default Student;
