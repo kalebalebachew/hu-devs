45c61388f369e56d570297ef049804672a2b489f
Kaleb <Kalebalebachew4@gmail.com>
2024-11-07 10:35:15 +0300

    chore: upcoming events update

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 const events = [
   {
-    title: "A Journey started in Haramaya University",
-    time: "Sunday, October 20 2:30 LT",
+    title: "Campus Innovation the Kuraztech's Journey",
+    time: "Saturday, November 9 2:30 LT",
     location: "HUDC Telegram Channel",
-    img: "/john.jpg",
+    img: "/tito.png",
     description:
-      "We’re excited to have Yohannes, a former Haramaya lecturer, student, and now a Senior Data Engineer at Excellerent Solutions.\n\nYohannes went from sitting in the same classrooms you're in as a student, to teaching in them as a lecturer, and now he’s working in the tech industry.\n\nIn this session, he’ll share:\n\n• How he moved from learning and teaching to tech.\n• The challenges he faced along the way.\n• Practical tips for anyone wanting to start in tech."
+      "HUDC is excited to bring you special live session featuring Tito Frezer, one of the key figures behind KurazTech —a company that started as a university club and grew into a successful tech company."
   },
 ];
 
