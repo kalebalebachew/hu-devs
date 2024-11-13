674f2bbd4e6cbb7ff23c2cb4a6df997e1d4c54f5
dagmawib <dagmawibelayneh14@gmail.com>
2024-11-13 19:06:03 +0300

    new upcoming event

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -3,13 +3,27 @@ import Image from "next/image";
 
 const events = [
   {
+    title:"Stories from gugt podcast, Freelancing and AASTU",
+    time: "Saturday, November 16 2:30 LT",
+    location: "HUDC Telegram Channel",
+    img: "/biruk.jpg",
+    description:"This time, we're hosting Brook Belihu ,the former co-host of Gugut Podcast —a name many of you already know and love! Now making waves with PulseERP, We will start right from the AASTU days, dive into his freelancing journey, and cover everything in between!"
+  },
+  {
+    title:"CTO AT KURAZTECH",
+    time: "Saturday, November 9 2:30 LT",
+    location: "HUDC Telegram Channel",
+    img: "/tito.jpg",
+    description: "The inspiration and early days of KurazTech.\n Tito’s journey as a student developer and the skills that shaped his career."
+  },
+  {
     title: "A Journey started in Haramaya University",
     time: "Sunday, October 20 2:30 LT",
     location: "HUDC Telegram Channel",
     img: "/john.jpg",
     description:
       "We’re excited to have Yohannes, a former Haramaya lecturer, student, and now a Senior Data Engineer at Excellerent Solutions.\n\nYohannes went from sitting in the same classrooms you're in as a student, to teaching in them as a lecturer, and now he’s working in the tech industry.\n\nIn this session, he’ll share:\n\n• How he moved from learning and teaching to tech.\n• The challenges he faced along the way.\n• Practical tips for anyone wanting to start in tech."
-  },
+  }
 ];
 
 export function UpcomingEventsTab() {
